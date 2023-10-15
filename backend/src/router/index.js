5a5e5938bce8456b5b687d45d88df702df6b8c52
Mulia Sujiastuti Wahyono <muliasujiastuti@gmail.com>
2023-10-15 09:28:05 +0700

    add reset password layout

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
 import Login from "../views/Login.vue";
+import RequestPassword from "../views/RequestPassword.vue";
+import ResetPassword from "../views/ResetPassword.vue";
 
 const routes = [
     {
@@ -13,6 +15,16 @@ const routes = [
         name: 'login',
         component: Login,
     },
+    {
+        path: '/request-password',
+        name: 'request-password',
+        component: RequestPassword,
+    },
+    {
+        path: '/request-password/token',
+        name: 'request-password-token',
+        component: ResetPassword,
+    }
 ];
 
 const router = createRouter({
